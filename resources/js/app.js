3b0500129dd94b132d7d46159e450a755d0193e0
shakil-lskit <sakil@lskit.com>
2022-03-25 18:56:02 -0700

    request progress bar added

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,7 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
-
-
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
@@ -16,3 +15,14 @@ createInertiaApp({
       .mount(el)
   },
 })
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 50,
+    // The color of the progress bar.
+    color: '#f5548e',
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+})
+
